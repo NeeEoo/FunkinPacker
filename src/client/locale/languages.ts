cebddf8feb0428a1838578ef44a063b9c0f22b63
Ne_Eo <NeeEoo@users.noreply.github.com>
2024-09-11 01:11:11 +0200

    Italian added
    Co-Authored-By: ⍚~Nex <87421482+nexisdumb@users.noreply.github.com>

--- src/client/locale/languages.ts
+++ src/client/locale/languages.ts
@@ -5,6 +5,7 @@ import * as ru from "./languages/ru.json";
 import * as zh from "./languages/zh-cn.json";
 import * as pt_br from "./languages/pt-br.json";
 import * as ca_fr from "./languages/ca-fr.json";
+import * as it from "./languages/it.json";
 
 export type Language = {
 	lang: string;
@@ -25,8 +26,9 @@ export const languages: Language[] = [
 	{ lang: "es", name: "español", mapping: es },
 	{ lang: "ru", name: "русский", mapping: ru },
 	{ lang: "zh-cn", name: "简体中文", mapping: zh },
-	{ lang: "pt-br", name: "português (brasil)", mapping: pt_br }, // by CrowPlexus
-	{ lang: "ca-fr", name: "français (canadien)", mapping: ca_fr }, // by thatoneidiotxav
+	{ lang: "pt-br", name: "português (brasil)", mapping: pt_br }, // by @CrowPlexus
+	{ lang: "ca-fr", name: "français (canadien)", mapping: ca_fr }, // by @thatoneidiotxav
+	{ lang: "it", name: "italiano", mapping: it }, // by @NexIsDumb
 ] as const;
 
 const ALL_KEYS = Object.keys(languages[0].mapping);
